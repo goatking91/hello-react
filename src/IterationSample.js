b6219732800c070b51a63ba0a833cb5b3511f2bb
염승민 <tmdals3021@gmail.com>
2019-09-13 11:08:34 +0900

    6.3 key

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class IterationSample extends Component {
   render() {
     const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map(value => <li>{value}</li>);
+    const nameList = names.map((value, index) => {
+      const indexPlus = index + 1;
+      return <li key={indexPlus}>{value}</li>;
+    });
     return <ul>{nameList}</ul>;
   }
 }
