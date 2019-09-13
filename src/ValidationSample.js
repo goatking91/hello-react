40b85de760d637da5ccdd20c993e0b91012d25ec
염승민 <tmdals3021@gmail.com>
2019-09-13 10:33:43 +0900

    5.1 ref는 어떤 상황에서 사용해야 할까?

---
+++ src/ValidationSample.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './ValidationSample.css';
+
+class ValidationSample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: '',
+      clicked: false,
+      validated: false,
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleButtonClick = () => {
+    const { password } = this.state;
+    this.setState({
+      clicked: true,
+      validated: password === '0000',
+    });
+  };
+
+  render() {
+    const { clicked, password, validated } = this.state;
+    const validatedClassName = validated ? 'success' : 'failure';
+    return (
+      <div>
+        <input
+          type="password"
+          value={password}
+          onChange={this.handleChange}
+          className={clicked ? validatedClassName : ''}
+        />
+        <button type="button" onClick={this.handleButtonClick}>
+          검증하기
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ValidationSample;
