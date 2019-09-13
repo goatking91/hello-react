47b458adcdc3592b882b6cd6fcea59aa85e2d06e
염승민 <tmdals3021@gmail.com>
2019-09-13 11:25:28 +0900

    6.4 응용

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'react/prefer-stateless-function': 0,
     'react/jsx-filename-extension': 0,
     'react/jsx-one-expression-per-line': 0,
+    'react/no-array-index-key': 0,
     'no-console': 0,
     'no-alert': 0,
   },
--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,13 +1,70 @@
 import React, { Component } from 'react';
 
 class IterationSample extends Component {
-  render() {
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map((value, index) => {
-      const indexPlus = index + 1;
-      return <li key={indexPlus}>{value}</li>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      names: ['눈사람', '얼음', '눈', '바람'],
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      name: e.target.value,
     });
-    return <ul>{nameList}</ul>;
+  };
+
+  handleInsert = () => {
+    // names 배열에 값을 추가하고, name 값을 초기화합니다.
+    const { name, names } = this.state;
+    this.setState({
+      names: names.concat(name),
+      name: '',
+    });
+  };
+
+  handleRemove = index => {
+    // 편의상 nmae의 레퍼런스를 미리 만듭니다.
+    const { names } = this.state;
+    /*
+    배열을 자르는 내장 함수 slice와
+    전개 연산자(...)를 사용하여 index번째 값을 제외한 값들을
+    배열에 넣어줍니다.
+    this.setState({
+      names: [
+        ...names.slice(0, index),
+        ...names.slice(index + 1, names.length),
+      ],
+    });
+     */
+
+    this.setState({
+      // filter로 index번째를 제외한 원소만 있는 새 배열 생성
+      names: names.filter((item, i) => i !== index),
+    });
+  };
+
+  render() {
+    const { name, names } = this.state;
+
+    const nameList = names.map((value, index) => {
+      return (
+        <li key={index} onDoubleClick={() => this.handleRemove(index)}>
+          {value}
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <input onChange={this.handleChange} value={name} />
+        <button type="button" onClick={this.handleInsert}>
+          추가
+        </button>
+        <ul>{nameList}</ul>
+      </div>
+    );
   }
 }
 
